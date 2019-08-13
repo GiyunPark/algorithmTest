5bd19e99fda2c617eb950658795f351d9ba6fd21
giyun <patronymic@naver.com>
2019-08-14 01:36:53 +0900

    Added 탑 메인 클래스 추가

---
+++ src/main/java/stackQue/top/TopMain.java
@@ -0,0 +1,24 @@
+package stackQue.top;
+
+public class TopMain {
+    public static void main(String[] args){
+        TopSolution topSolution=new TopSolution();
+
+        int[] heights={6,9,5,7,4};
+        int[] answer= topSolution.solution(heights);
+
+        int[] heights1={3,9,9,3,5,7,2};
+        int[] answer1= topSolution.solution(heights1);
+
+        int[] heights2={1,5,3,6,7,6,5};
+        int[] answer2= topSolution.solution(heights2);
+
+
+        ResultView resultView= new ResultView();
+
+        resultView.showResult(answer);
+        resultView.showResult(answer1);
+        resultView.showResult(answer2);
+
+    }
+}
