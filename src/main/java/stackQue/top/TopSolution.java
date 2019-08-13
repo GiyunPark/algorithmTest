99ca1fcae7ca6f9cdc66cf120c1109067e30d090
giyun <patronymic@naver.com>
2019-08-14 01:36:19 +0900

    Feat 스택에의한 송수신 기능 추가

--- src/main/java/stackQue/top/TopSolution.java
+++ src/main/java/stackQue/top/TopSolution.java
@@ -1,4 +1,33 @@
 package stackQue.top;
 
-public class TopSolution {
+import java.util.Stack;
+
+class TopSolution {
+    public int[] solution(int[] heights) {
+        Stack<Integer> sender=new Stack<>();
+        Stack<Integer> receiver=new Stack<>();
+        int[] answer = new int[heights.length];
+
+        for(int sendIdx=0; sendIdx<heights.length; sendIdx++){
+            sender.push(heights[sendIdx]);
+        }
+        for(int heightsLen=heights.length; heightsLen>0; heightsLen-- ){
+
+            for(int receiverIdx=0; receiverIdx<heightsLen-1; receiverIdx++){
+                receiver.push(heights[receiverIdx]);
+            }
+            int senderHeight=sender.pop();
+
+            for(int receiverIdx=heightsLen-1; receiverIdx>0; receiverIdx--){
+                int receiverHeight=receiver.pop();
+
+                if(senderHeight<receiverHeight){
+                    answer[heightsLen-1]=receiverIdx;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
