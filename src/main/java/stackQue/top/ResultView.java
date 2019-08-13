2adec22dbaa74971cf1d9ba081e5870141c64043
giyun <patronymic@naver.com>
2019-08-14 01:37:16 +0900

    Feat 수신 결과 보여주는 기능 추가

---
+++ src/main/java/stackQue/top/ResultView.java
@@ -0,0 +1,11 @@
+package stackQue.top;
+
+import java.util.Arrays;
+
+public class ResultView {
+
+    public void showResult(int[] answer){
+        System.out.println("return");
+        System.out.println(Arrays.toString(answer));
+    }
+}
